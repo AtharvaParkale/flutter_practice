b50961b67bfad16dbce48e87be71a924cc22c7e4
Atharva Parkale <xatharvaparkale@freo.money>
2024-12-25 13:24:11 +0530

    change app title

--- lib/features/home/presentation/pages/home_page.dart
+++ lib/features/home/presentation/pages/home_page.dart
@@ -24,7 +24,7 @@ class _HomePageState extends State<HomePage> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: const Text('Blog App'),
+        title: const Text('Topics'),
         actions: [
           IconButton(
             onPressed: () {},
